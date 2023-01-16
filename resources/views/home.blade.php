bf78542b05aba709c441ddc0d7606a8df815af9d
DiceglieA <angelodiceglie10@gmail.com>
2023-01-16 16:54:50 +0100

    last commit + bonus (imported npm bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 @section('content')
-    @foreach ($movies as $movie)
-        <h1>{{ $movie['title']}}</h1>
-    @endforeach
+    <h1 class="text-center">Movies</h1>
+    {{-- insert movies from db --}}
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $elem)
+                    <div class="col-3 g-2">
+                        <div class="card bg-secondary" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$elem['title']}}</h5>
+                                <p class="card-text">Nationality: {{$elem['nationality']}}</p>
+                                <p class="card-text">Date: {{$elem['date']}}</p>
+                                <p class="card-text">Date: {{$elem['vote']}}</p>
+                            </div>
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+
+    </div>
 @endsection
